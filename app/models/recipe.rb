32b2de6f7fa07764401b3d551c66b1ac9afa86b9
Tim Ruffles <timruffles@Tims-Laptop.local>
2010-07-14 18:04:20 +0100

    Got a simple step thing that makes links clickable and splits up list

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,18 @@
 class Recipe < ActiveRecord::Base
   belongs_to :chef
+  
+  
+  def each_step
+    part_no = 0
+    steps.split(/^\d+\./).each do |part|
+      next if part =~ /^[\s\t\r]*$/
+      part_no += 1
+      yield make_links(part), part_no
+    end
+  end
+  
+  def make_links part
+    part.gsub(/(http:\/\/\S*)/,'<a href="\1">Click here</a>')
+  end
+  
 end
