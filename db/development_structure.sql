afedbc6b3ad12b7cdc1e12376cfd2f58e2dc68c2
Tim Ruffles <timruffles@Tims-Laptop.lan>
2010-07-18 21:25:59 +0100

    Changes to lots of the app

--- db/development_structure.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO schema_migrations (version) VALUES ('20100714114633');
-
-INSERT INTO schema_migrations (version) VALUES ('20100718092323');
-
-INSERT INTO schema_migrations (version) VALUES ('20100718093108');
-
-INSERT INTO schema_migrations (version) VALUES ('20100718095708');
